abf4459b120142c631bc9437217aea5d4a00242e
DevSantiro <65986475+DevSantiro@users.noreply.github.com>
2021-05-18 21:09:56 -0300

    Primeiros passos + construindo navbar

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Navegacao = styled.div`
+  background-color: #051D3B;
+  display: flex;
+  justify-content: space-between; 
+  padding: 0.2rem;
+`;
